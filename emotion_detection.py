b43de108710ee8bb43701807d1349da7d3578ae9
Drakunal <drakunaldey1@gmail.com>
2024-07-19 00:40:40 -0400

    output modified

--- emotion_detection.py
+++ emotion_detection.py
@@ -9,13 +9,13 @@ def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }  
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    response = requests.post(url, json = myobj, headers=header)  
+    response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    anger_score = formatted_response['emotion']['anger']
-    disgust_score = formatted_response['emotion']['disgust']
-    fear_score = formatted_response['emotion']['fear']
-    joy_score = formatted_response['emotion']['joy']
-    sadness_score = formatted_response['emotion']['sadness']
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
     max_score = max(anger_score, disgust_score, fear_score, joy_score, sadness_score)
     if(max_score == anger_score):
         dominant = 'anger'
