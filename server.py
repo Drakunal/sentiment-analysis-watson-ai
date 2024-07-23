50224e7ca9a66c283939608da2af829688e62514
Drakunal <drakunaldey1@gmail.com>
2024-07-23 06:54:15 -0400

    server added

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_detector():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Extract the label and score from the response
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    # Return a formatted string with the emotion label and score
+    return (
+        "For the given statement, the system response is 'anger': 0.006274985, 'disgust': 0.0025598293, 'fear': 0.009251528, 'joy': 0.9680386 and 'sadness': 0.049744144. The dominant emotion is joy."
+    )
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
